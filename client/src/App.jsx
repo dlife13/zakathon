667eaeda7250eb0e538b7e9f2e89c3a3cc7aea42
Harsh <harshbhatia1605@gmail.com>
2024-10-19 12:09:51 +0530

    format

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,27 @@
-import { useState, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
-import axios from "axios";
-import TablePage from "./TablePage";
-import HomePage from "./HomePage";
-import SampleTable from "./SampleTable";
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './HomePage'
+import SampleTable from './SampleTable'
 
 function App() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([])
 
   const fetchAPI = async () => {
-    const response = await axios.get("http://localhost:8080/api/users");
-    setUsers(response.data);
-  };
+    const response = await axios.get('http://localhost:8080/api/users')
+    setUsers(response.data)
+  }
 
   useEffect(() => {
-    fetchAPI();
-  }, []);
+    fetchAPI()
+  }, [])
 
   return (
     <Routes>
-      <Route path="/all" element={<SampleTable />} />
-      <Route path="/" element={<HomePage />} />
+      <Route path='/all' element={<SampleTable />} />
+      <Route path='/' element={<HomePage />} />
     </Routes>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/src/components/UserTable.jsx
+++ client/src/components/UserTable.jsx
@@ -1,4 +1,13 @@
-import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
 
 export default function UserTable({ users }) {
   return (
@@ -6,34 +15,36 @@ export default function UserTable({ users }) {
       <TableCaption>A list of users and their ratings.</TableCaption>
       <TableHeader>
         <TableRow>
-          <TableHead className="text-left">Handle</TableHead>
-          <TableHead className="text-left">Name</TableHead>
-          <TableHead className="text-left">BITS ID</TableHead>
-          <TableHead className="text-left">Branch</TableHead>
-          <TableHead className="text-right">Peak Rating</TableHead>
-          <TableHead className="text-right">Current Rating</TableHead>
+          <TableHead className='text-left'>Handle</TableHead>
+          <TableHead className='text-left'>Name</TableHead>
+          <TableHead className='text-left'>BITS ID</TableHead>
+          <TableHead className='text-left'>Branch</TableHead>
+          <TableHead className='text-right'>Peak Rating</TableHead>
+          <TableHead className='text-right'>Current Rating</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {users.map((user) => (
+        {users.map(user => (
           <TableRow key={user.bitsId}>
-            <TableCell className="text-left font-medium">{user.handle}</TableCell>
-            <TableCell className="text-left">{user.name}</TableCell>
-            <TableCell className="text-left">{user.bitsId}</TableCell>
-            <TableCell className="text-left">{user.branch}</TableCell>
-            <TableCell className="text-right">{user.peakRating}</TableCell>
-            <TableCell className="text-right">{user.currentRating}</TableCell>
+            <TableCell className='text-left font-medium'>
+              {user.handle}
+            </TableCell>
+            <TableCell className='text-left'>{user.name}</TableCell>
+            <TableCell className='text-left'>{user.bitsId}</TableCell>
+            <TableCell className='text-left'>{user.branch}</TableCell>
+            <TableCell className='text-right'>{user.peakRating}</TableCell>
+            <TableCell className='text-right'>{user.currentRating}</TableCell>
           </TableRow>
         ))}
       </TableBody>
       <TableFooter>
         <TableRow>
-          <TableCell colSpan={5} className="text-left">
+          <TableCell colSpan={5} className='text-left'>
             Total Users
           </TableCell>
-          <TableCell className="text-right">{users.length}</TableCell>
+          <TableCell className='text-right'>{users.length}</TableCell>
         </TableRow>
       </TableFooter>
     </Table>
-  );
+  )
 }
